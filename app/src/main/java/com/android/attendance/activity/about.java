16173e52d313835d6fc9f0f959bfd086389ed204
ainnadhiraah <naaadhirah02@gmail.com>
2024-01-28 00:40:04 +0800

    Inital Files

---
+++ app/src/main/java/com/android/attendance/activity/about.java
@@ -0,0 +1,17 @@
+package com.android.attendance.activity;
+
+import android.app.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import com.example.androidattendancesystem.R;
+
+
+
+public class about extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
